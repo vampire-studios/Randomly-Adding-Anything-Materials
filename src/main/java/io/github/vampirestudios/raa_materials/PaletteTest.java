85b7eabb036c3dd14dbfae98a7580d2a5648bdc0
Maddy <22009843+AurySystem@users.noreply.github.com>
2022-04-12 18:24:29 -0400

    more corrosions stuff

--- src/main/java/io/github/vampirestudios/raa_materials/PaletteTest.java
+++ src/main/java/io/github/vampirestudios/raa_materials/PaletteTest.java
@@ -1,5 +1,6 @@
 package io.github.vampirestudios.raa_materials;
 
+import io.github.vampirestudios.raa_materials.utils.ColorDualGradient;
 import io.github.vampirestudios.raa_materials.utils.ColorGradient;
 import io.github.vampirestudios.raa_materials.utils.ProceduralTextures;
 
@@ -15,15 +16,18 @@ public class PaletteTest {
         int paletteSize = 7;
         int palettes = 32;
 
-        ColorGradient mat;
-        BufferedImage img = new BufferedImage(paletteSize * 32, palettes * 32, BufferedImage.TYPE_INT_ARGB);
+        //ColorGradient mat;
+        ColorDualGradient mat;
+        BufferedImage img = new BufferedImage(paletteSize * 32, palettes * 3 * 32, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img.createGraphics();
         for(int y = 0; y < palettes; y++) {
-            mat = ProceduralTextures.makeGemPalette(random);
+            mat = ProceduralTextures.makeMetalPalette(random);
             for (int i = 0; i < paletteSize; i++) {
                 int x = i * 32;
-                g2d.setColor(new Color(mat.getColor((float) i / paletteSize).getAsInt()));
-                g2d.fillRect(x, y*32, 32, 32);
+                for(int z = 0; z < 3; z++){
+                g2d.setColor(new Color(mat.getColor((float) i / paletteSize, (float) z/2).getAsInt()));
+                g2d.fillRect(x, (y*3+z)*32, 32, 32);
+                }
                 //System.out.println(ColorUtil.toHexString(palette[i]));
             }
         }
